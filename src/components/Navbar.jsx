28bef5815ca86a4ebdd761126f329b6908189576
CarlosGallegos <gallegosgutierrezj62@gmail.com>
2025-12-12 23:22:14 -0600

    UI: Show Google user avatar in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,10 +19,18 @@ export default function Navbar() {
 
                 {user ? (
                     <div className="flex items-center gap-4">
-                        <Link to="/profile" className="flex items-center justify-center w-10 h-10 rounded-full bg-zinc-800 text-green-500 hover:bg-zinc-700 hover:text-green-400 transition-all border border-zinc-700 shadow-md group" title="Tu Perfil">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 transform group-hover:scale-110 transition-transform">
-                                <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-                            </svg>
+                        <Link to="/profile" className="relative flex items-center justify-center w-10 h-10 rounded-full overflow-hidden bg-zinc-800 text-green-500 hover:bg-zinc-700 hover:text-green-400 transition-all border border-zinc-700 shadow-md group" title="Tu Perfil">
+                            {user?.user_metadata?.avatar_url ? (
+                                <img
+                                    src={user.user_metadata.avatar_url}
+                                    alt="Avatar"
+                                    className="w-full h-full object-cover"
+                                />
+                            ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 transform group-hover:scale-110 transition-transform">
+                                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                                </svg>
+                            )}
                         </Link>
                     </div>
                 ) : (
